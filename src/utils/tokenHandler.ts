a5fb049d941a4b8a6498ccb84e3622cdb565840d
Aseer KT <aseermiqdad@gmail.com>
2021-03-06 12:09:15 +0530

    moved jwt secret into .env

--- src/utils/tokenHandler.ts
+++ src/utils/tokenHandler.ts
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 import { User } from '../entities/User';
-const TOKEN_SECRET = 'joiasdifojaiosdjfjdfsaoifjds1312mu,@*#*#';
+const TOKEN_SECRET = process.env.JWT_SECRET!;
 
 export const createToken = (user: User) => {
   return jwt.sign({ username: user.username }, TOKEN_SECRET, {
