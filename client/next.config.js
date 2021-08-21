fc82021d036dfebcd339820ba40012f98f2ee84f
Aseer KT <aseermiqdad@gmail.com>
2021-08-21 09:36:09 +0530

    fix: svgr webpack next config

--- client/next.config.js
+++ client/next.config.js
@@ -2,9 +2,7 @@ module.exports = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
-      },
+      issuer: { and: [/\.(js|ts)x?$/] },
       use: ['@svgr/webpack'],
     });
 
