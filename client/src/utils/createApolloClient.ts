2414f4de83efc1558724da50190cc23eb3548b5c
Aseer KT <aseermiqdad@gmail.com>
2021-08-22 19:28:59 +0530

    chore: export cache from client config

--- client/src/utils/createApolloClient.ts
+++ client/src/utils/createApolloClient.ts
@@ -14,29 +14,31 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
   }
 });
 
-export function createApolloClient() {
-  return new ApolloClient({
-    link: ApolloLink.from([errorLink, httpLink]),
-    cache: new InMemoryCache({
-      typePolicies: {
-        Query: {
-          fields: {
-            getPosts: {
-              // Don't cache separate results based on
-              // any of this field's arguments.
-              keyArgs: [],
-              // Concatenate the incoming list items with
-              // the existing list items.
-              merge(existing: PaginatedPost, incoming: PaginatedPost) {
-                return {
-                  ...incoming,
-                  posts: [...(existing?.posts || []), ...incoming.posts],
-                };
-              },
-            },
+export const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        getPosts: {
+          // Don't cache separate results based on
+          // any of this field's arguments.
+          keyArgs: [],
+          // Concatenate the incoming list items with
+          // the existing list items.
+          merge(existing: PaginatedPost, incoming: PaginatedPost) {
+            return {
+              ...incoming,
+              posts: [...(existing?.posts || []), ...incoming.posts],
+            };
           },
         },
       },
-    }),
+    },
+  },
+});
+
+export function createApolloClient() {
+  return new ApolloClient({
+    link: ApolloLink.from([errorLink, httpLink]),
+    cache,
   });
 }
