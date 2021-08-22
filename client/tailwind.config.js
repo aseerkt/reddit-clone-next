3c466f671fdf168851db8911046e2efc2bf05b2b
Aseer KT <aseermiqdad@gmail.com>
2021-08-22 21:51:31 +0530

    chore: add shared directory tailwind purge

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/utils/**/*.{js,ts,jsx,tsx}',
+    './src/shared/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
