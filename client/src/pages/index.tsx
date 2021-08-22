91bde346f293db803b24ec6d41d72329b2821fd1
Aseer KT <aseermiqdad@gmail.com>
2021-08-22 19:30:32 +0530

    chore: extracted head tag in home page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -10,15 +10,27 @@ const title = 'lireadit';
 const description =
   "Reddit is a network of communities based on people's interests. Find communities you're interested in, and become part of an online community!";
 
+const HeadContents = () => (
+  <Head>
+    <title>lireadit: the fake page of the internet</title>
+    <meta name='description' content={description} />
+    <meta property='og:title' content={title} />
+    <meta property='og:description' content={description} />
+    <meta property='twitter:title' content={title} />
+    <meta property='twitter:description' content={description} />
+  </Head>
+);
+
 export default function Home() {
   const { data, loading, fetchMore, error, variables, networkStatus } =
     useGetPostsQuery({
       variables: { limit: 8, offset: 0 },
       notifyOnNetworkStatusChange: true,
     });
-  console.log(networkStatus);
+
   if (loading) {
     <div className='container h-full pt-4 text-center'>
+      <HeadContents />
       <div className='animate-spin'>
         <SpinnerSvg />
       </div>
@@ -28,6 +40,7 @@ export default function Home() {
   if (!loading && !data) {
     return (
       <div className='container h-full pt-4 text-center'>
+        <HeadContents />
         <h1 className='text-2xl font-bold text-red-800'>
           Oops, Something went wrong
         </h1>
@@ -39,14 +52,7 @@ export default function Home() {
 
   return (
     <>
-      <Head>
-        <title>lireadit: the fake page of the internet</title>
-        <meta name='description' content={description} />
-        <meta property='og:title' content={title} />
-        <meta property='og:description' content={description} />
-        <meta property='twitter:title' content={title} />
-        <meta property='twitter:description' content={description} />
-      </Head>
+      <HeadContents />
       <div className='container flex pt-4'>
         {/* Post Feed */}
         <div className='w-160'>
@@ -56,13 +62,13 @@ export default function Home() {
               <PostCard key={post.id} post={post as Post} />
             ))}
             {networkStatus === 3 && (
-              <div className='flex justify-center py-6 mb-10 bg-gray-400 rounded-sm'>
+              <div className='flex justify-center py-6 mb-10 rounded-sm'>
                 <div className='mt-4 text-gray-500 animate-none'>
                   <SpinnerSvg />
                 </div>
               </div>
             )}
-            {data && data.getPosts.hasMore && (
+            {data?.getPosts?.hasMore && (
               <Waypoint
                 onEnter={() => {
                   fetchMore({
